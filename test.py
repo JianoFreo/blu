4b341aa6976f0318fd6b6b9b66248fd38ffcdc48
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-02 22:32:31 +0800

    test on py files

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-import torch
+from imports import *
+
+block_size = 8
+
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print('when input is', context, 'target is', target)
